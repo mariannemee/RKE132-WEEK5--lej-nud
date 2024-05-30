4fa685def54cc06b464eabbdc3e171715daa1cef
Mari-Anne Mee <mari-anne@MariAnnesLaptop.lan>
2024-05-30 14:05:21 +0300

    create new app

---
+++ Program copy.cs
@@ -0,0 +1,33 @@
+﻿
+int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+if(weekDay == 0)
+{
+    Console.WriteLine("It's Sunday!");
+}
+else
+{
+    Console.WriteLine("It's Monday!");
+}
+else
+{
+    Console.WriteLine("It's Tuesday!");
+}
+else
+{
+    Console.WriteLine("It's Wednesday");
+}
+else
+{
+    Console.WriteLine("It's Thursday!");
+}
+else
+{
+    Console.WriteLine("It's Friday!");
+}
+else
+{
+    Console.WriteLine("It's Saturday!");
+}
+
+Console.WriteLine("Have a nice day!");
